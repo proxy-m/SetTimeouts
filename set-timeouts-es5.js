feaa1710686c9d7df05c3458a098ace267c614f4
proxy-m <wproxym@gmail.com>
2020-04-10 13:20:10 +0300

    startLastAndFinish es5

--- set-timeouts-es5.js
+++ set-timeouts-es5.js
@@ -1,16 +1,16 @@
 'use strict';
 /**
-  SetTimeouts: version: 1.3
+  SetTimeouts: version: 1.4
   License: MIT
 */
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
 
 function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 var SetTimeouts = /*#__PURE__*/function () {
   /**
@@ -128,10 +128,32 @@ var SetTimeouts = /*#__PURE__*/function () {
           }
 
           _this2.functionToCall = _this2.functionToCallOrig;
+
           _this2.start();
         };
       } else if (!doNotWarn) {
         console.warn('startSoft already initiated, no need additional');
+      }
+    }
+    /**
+     * Start one more last time and stop.
+     * Warning: it destroys settings of repeatLastTimeout and timeouts
+     */
+
+  }, {
+    key: "startLastAndFinish",
+    value: function startLastAndFinish(doNotImmediate, doWarn) {
+      if (!doNotImmediate || this.repeatLastTimeout) {
+        this.stop();
+      }
+
+      this.repeatLastTimeout = false;
+      this.timeouts = [0];
+
+      if (!doNotImmediate) {
+        this.start();
+      } else {
+        this.startSoft(!doWarn);
       }
     }
   }]);
